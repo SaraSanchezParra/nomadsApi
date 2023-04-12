25a49942ff5443b29ba295bab2ce9915ca2f8639
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-04-12 13:24:10 +0200

    Corrección de errores (no se leer)

--- src/app.js
+++ src/app.js
@@ -2,20 +2,21 @@ const express = require("express")
 const cors = require ("cors")
 const userRouters = require ("./routers/user.routers")
 const viajeRouters = require ("./routers/viajes.routers.js")
+const errorHandling = require("./error/errorHandling")
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000)
 app.use(cors());
-app.use(express.url.encoded({extended: false}));
-app.use(expres.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use(function(req, res, next){
     res.status(404).json({error:true, 
                           codigo:404,
                           message: "End point doesn't found"})
 })
 
-app.use(erroHandling);
+app.use(errorHandling);
 module.exports = app
 
 
