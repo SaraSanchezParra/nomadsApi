728d9c831e1a9a382d278b8c4374ed9b959158a5
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-04-12 17:57:44 +0200

    modificaciones

---
+++ src/controller/user.controller.js
@@ -0,0 +1,38 @@
+const connection = require("../database")
+
+
+
+
+function getStart(request, response) {
+    let respuesta = {error: true, codigo: 200, mensaje: 'Beginning point'};
+    response.send(respuesta);
+}
+
+function getUser(request,response){
+
+    let user_id = request.query.user_id;
+    let params;
+    let sql;
+    console.log(request.query);
+
+    params= [user_id]
+    sql = `SELECT username, name, surname, descripcion, foto FROM user WHERE user_id = ?` ;
+    
+   
+    
+    connection.query(sql,params,(err,res)=>{
+      
+        if(res.length > 0){
+          respuesta = {error:false, codigo:200, mensaje:'Usuario encontrado', data:res}
+        } else {
+          console.log('Los datos proporcionados no coinciden con ningún usuario en la base de datos.')
+          respuesta = {error:true, codigo:200, mensaje:'No encontrado', data:res}
+        }
+        console.log(respuesta);
+      
+      response.send(respuesta)
+    })
+  } 
+  
+  
+module.exports = {getStart, getUser}
---
+++ src/routers/user.routers.js
@@ -0,0 +1,9 @@
+const {Router} = require ("express");
+const router = Router();
+const apiUserCtrl = require("../controller/user.controller");
+
+
+router.get("/", apiUserCtrl.getStart);
+router.get("/user", apiUserCtrl.getUser)
+
+module.exports = router
