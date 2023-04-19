960a1d3b4d87762cef471eecba8691f2193c0b63
juliop11 <julioperezobrero@hotmail.com>
2023-04-19 11:21:56 +0200

    JULIO

--- src/routers/viajes.routers.js
+++ src/routers/viajes.routers.js
@@ -11,7 +11,7 @@ router.get("/topViajesLog", viajesCTRL.getTopViajesLog);
 
 router.get("/topNomads", viajesCTRL.getTopNomads);
 
-router.get("/viaje", viajesCTRL.getDiasOfViaje);
+// router.get("/viaje", viajesCTRL.getDiasOfViaje);
 
 router.get("/getPI", viajesCTRL.getPIOfDay);
 
