e1762bc566b3c89401873dd6bf1520ee57039db6
juliop11 <julioperezobrero@hotmail.com>
2023-04-20 23:42:21 +0200

    comprobar username

--- src/controller/register.controller.js
+++ src/controller/register.controller.js
@@ -2,27 +2,26 @@ const connection = require("../database")
 const CryptoJS = require("crypto-js");
 
 
-function getRegister(request,response){
+function getRegister(request, response) {
 
   let sql;
-  if (request.query.user_id == null) 
-  sql = "SELECT * FROM user";
+  if (request.query.user_id == null)
+    sql = "SELECT * FROM user";
   else sql = "SELECT username,name,surname,email,password FROM user WHERE user_id = " + request.query.user_id;
 
   connection.query(sql, function (err, result) {
     console.log("se ha buscado");
-    if (err) 
-    console.log(err);
+    if (err)
+      console.log(err);
     else {
-      
+
       response.send(result);
     }
   });
 }
 
 
-function postRegister(request,response)
-{
+function postRegister(request, response) {
   console.log(request.body);
   let foto;
   if (request.body.photo != null) {
@@ -35,31 +34,47 @@ function postRegister(request,response)
   const KEY = '123456$#@$^@1ERF%&€'
   let password = request.body.password;
   let hash = CryptoJS.HmacSHA256(password, KEY);
-  let passwordCifrado= CryptoJS.enc.Base64.stringify(hash); 
+  let passwordCifrado = CryptoJS.enc.Base64.stringify(hash);
   console.log(passwordCifrado);
-  
-  let sql = "INSERT INTO user (username,name,surname,email,descripcion,password,photo)" + 
-  "VAlUES ('" + request.body.username + "','"+ 
-                request.body.name + "', '" +
-                request.body.surname + "', '" +  
-                request.body.email + "','', '" + 
-                passwordCifrado + "','" + 
-                foto + "')"
-  // console.log(sql);
 
-  
-  connection.query(sql,function(err,res)
-  {
-    if(err){
-      console.log(err);
-      respuesta = {error:true, codigo:200, mensaje:'No registrado', data_user:res}
+  let username = request.body.username;
 
-    }else{
+  let sqlCheckUser = "SELECT COUNT(*) as count FROM user WHERE username='" + username + "'";
+  connection.query(sqlCheckUser, function (err, result) {
+    if (err) {
       console.log(err);
-      respuesta = {error:false, codigo:200, mensaje:'registrado', data_user:res}
+      respuesta = { error: true, codigo: 200, mensaje: 'No registrado', data_user: result }
+      response.json(respuesta)
     }
-    response.json(respuesta)
-  })             
-}
+    else if (result[0].count > 0) {
+      console.log("Nombre de usuario ya existe");
+      respuesta = { error: true, codigo: 200, mensaje: 'Nombre de usuario ya existe', data_user: result }
+      response.json(respuesta)
+    }
+    else {
 
-module.exports={getRegister,postRegister};
+      let sql = "INSERT INTO user (username,name,surname,email,descripcion,password,photo)" +
+        "VAlUES ('" + request.body.username + "','" +
+        request.body.name + "', '" +
+        request.body.surname + "', '" +
+        request.body.email + "','', '" +
+        passwordCifrado + "','" +
+        foto + "')"
+      // console.log(sql);
+
+
+      connection.query(sql, function (err, res) {
+        if (err) {
+          console.log(err);
+          respuesta = { error: true, codigo: 200, mensaje: 'No registrado', data_user: res }
+
+        } else {
+          console.log(err);
+          respuesta = { error: false, codigo: 200, mensaje: 'registrado', data_user: res }
+        }
+        response.json(respuesta)
+      })
+    }
+  });
+}
+module.exports = { getRegister, postRegister };
