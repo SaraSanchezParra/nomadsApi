483a8f6d93ae51b9fc0048ba56f272ecac71236e
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-04-25 12:08:08 +0200

    Prueba 3 Versel

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const errorHandling = require("./error/errorHandling")
 const bodyParser = require('body-parser');
 const app = express();
 
-app.set("port", process.env.PORT || 3000)
+// app.set("port", process.env.PORT || 3000)
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
