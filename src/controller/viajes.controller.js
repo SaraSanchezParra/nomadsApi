702224be7f020da092bf546a2b1c30e8d6d4eb93
Amber C <amberpomar@gmail.com>
2023-04-26 11:53:10 +0200

    Res

--- src/controller/viajes.controller.js
+++ src/controller/viajes.controller.js
@@ -280,9 +280,7 @@ function postDia(req, response) {
   connection.query(sql, values, (err, result) => {
     if (err) {
       console.error(err);
-      res
-        .status(500)
-        .json({ error: true, codigo: 500, mensaje: "0", data: null });
+      answer = ({ error: true, codigo: 500, mensaje: "0", data: null });
     } else {
       let sqlD = "UPDATE nomads.viajes SET n_dias_viaje = n_dias_viaje + 1 WHERE viaje_id = ?";
       let paramsD = [req.body.viaje_id];
