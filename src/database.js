e08e014808baa64b8f38b0023a5160291b421c81
Sara Sanchez <saritt@MacBook-Air-de-Sara.local>
2023-05-04 13:32:23 +0200

    Trabajando con Pools

--- src/database.js
+++ src/database.js
@@ -1,20 +1,21 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
                 host : process.env.DB_HOST || "nomads.caxuwrjvrlwf.eu-north-1.rds.amazonaws.com",
                 user : process.env.DB_USER || "admin",
                 password : process.env.DB_PASSWORD || "Nomads1!",
                 database : process.env.DB_NAME || "nomads",
-                port : process.env.DB_PORT || 3306
+                port : process.env.DB_PORT || 3306,
+                connectionLimit: 10
 });
 
-connection.connect (function (error){
-    if (error){
-        console.log(error);
-    }
-    else{
-        console.log("Conexión correcta");
-    }
-});
+// connection.connect (function (error){
+//     if (error){
+//         console.log(error);
+//     }
+//     else{
+//         console.log("Conexión correcta");
+//     }
+// });
 
 module.exports = {connection};
